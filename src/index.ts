2af1fe1fb1391288ed0430700ad400c006aca98e
Carlos Martínez González <c.martinez.tdl@gmail.com>
2022-08-08 08:12:05 +0200

    refactor: moves canvas generation to a function

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
 import './index.css'
+import { addCanvas } from './utils/dom/addCanvas'
 
 window.addEventListener('load', () => {
   const rootElement = document.getElementById('root')
-  const canvas = document.createElement('canvas')
-  canvas.style['border'] = '3px double black'
-  canvas.style['position'] = 'absolute'
-  canvas.style['top'] = '50%'
-  canvas.style['left'] = '50%'
-  canvas.style['transform'] = 'translate(-50%, -50%)'
-  canvas.width = 500
-  canvas.height = 500
-  rootElement.append(canvas)
+  const canvas = addCanvas(rootElement)
 })
---
+++ src/utils/dom/addCanvas.ts
@@ -0,0 +1,12 @@
+export function addCanvas(rootElement: HTMLElement) {
+  const canvas = document.createElement('canvas');
+  canvas.style['border'] = '3px double black';
+  canvas.style['position'] = 'absolute';
+  canvas.style['top'] = '50%';
+  canvas.style['left'] = '50%';
+  canvas.style['transform'] = 'translate(-50%, -50%)';
+  canvas.width = 500;
+  canvas.height = 500;
+  rootElement.append(canvas);
+  return canvas;
+}
