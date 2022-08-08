208e12d8d93b4720ddb141e90ebbf8660ac86af1
Carlos Martínez González <c.martinez.tdl@gmail.com>
2022-08-08 08:04:35 +0200

    feat: adds main game canvas

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-import "./index.css"
+import './index.css'
 
-console.log('Hello world')
+window.addEventListener('load', () => {
+  const rootElement = document.getElementById('root')
+  const canvas = document.createElement('canvas')
+  canvas.style['border'] = '3px double black'
+  canvas.style['position'] = 'absolute'
+  canvas.style['top'] = '50%'
+  canvas.style['left'] = '50%'
+  canvas.style['transform'] = 'translate(-50%, -50%)'
+  canvas.width = 500
+  canvas.height = 500
+  rootElement.append(canvas)
+})
