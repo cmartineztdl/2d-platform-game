642ce732d1aca76e33185d55a845f120f101d83f
Carlos Martínez González <c.martinez.tdl@gmail.com>
2022-08-09 13:14:23 +0200

    feat: adds sprite animation with state machine

--- src/game/Player.ts
+++ src/game/Player.ts
@@ -1,8 +1,16 @@
 import { Game } from './Game'
 import playerSpritesheet from '../assets/spritesheets/player.png'
 import { ACTION } from './InputHandler'
+import {
+  Player as PlayerState,
+  StateName,
+  State,
+  SittingState,
+  RunningState,
+  JumpingState,
+} from './State'
 
-export class Player {
+export class Player implements PlayerState {
   private x: number
   private y: number
   private scale: number = 1
@@ -12,21 +20,35 @@ export class Player {
   private spriteSheetFrameHeight: number = 913 / 10
   private spriteSheetImage: HTMLImageElement = new Image()
   private spriteSheetFrameX: number = 0
-  private spriteSheetFramey: number = 0
+  private spriteSheetFrameY: number = 0
+  private numberOfSpriteSheets: number[] = [7, 7, 7, 9, 11, 5, 7, 7, 14, 4]
+  private spriteSheetAnimationInterval: number = 100
+  private timeSinceLastSpriteSheetAnimation: number = 0
   private speedX: number = 0
   private maxSpeedX: number = 0.3
   private speedY: number = 0
   private maxSpeedY: number = 2.5
   private weight: number = 0.01
+  private states: { [key in StateName]: State }
+  private currentState: State
 
   constructor(private game: Game) {
     this.width = this.spriteSheetFrameWidth * this.scale
     this.height = this.spriteSheetFrameHeight * this.scale
     this.x = 0
     this.y = this.game.height - this.height
+
+    this.states = {
+      sitting: new SittingState(this),
+      running: new RunningState(this),
+      jumping: new JumpingState(this),
+    }
+    this.setState('sitting')
   }
 
   public update(deltaTime: number) {
+    this.currentState.handleInput(this.game.inputHandler)
+
     if (this.game.inputHandler.getIsActionActive(ACTION.LEFT)) {
       this.speedX = -this.maxSpeedX
     } else if (this.game.inputHandler.getIsActionActive(ACTION.RIGHT)) {
@@ -50,13 +72,25 @@ export class Player {
     this.y = Math.min(this.game.height - this.height, this.y)
 
     this.speedY = this.isOnGround ? 0 : this.speedY + this.weight * deltaTime
+
+    this.timeSinceLastSpriteSheetAnimation += deltaTime
+    if (
+      this.timeSinceLastSpriteSheetAnimation >=
+      this.spriteSheetAnimationInterval
+    ) {
+      this.spriteSheetFrameX =
+        (this.spriteSheetFrameX + 1) %
+        this.numberOfSpriteSheets[this.spriteSheetFrameY]
+      this.timeSinceLastSpriteSheetAnimation -=
+        this.spriteSheetAnimationInterval
+    }
   }
 
   public draw() {
     this.game.context.drawImage(
       this.spriteSheetImage,
       this.spriteSheetFrameX * this.spriteSheetFrameWidth,
-      this.spriteSheetFramey * this.spriteSheetFrameHeight,
+      this.spriteSheetFrameY * this.spriteSheetFrameHeight,
       this.spriteSheetFrameWidth,
       this.spriteSheetFrameHeight,
       this.x,
@@ -73,7 +107,20 @@ export class Player {
     })
   }
 
-  private get isOnGround() {
+  public get isOnGround() {
     return this.y >= this.game.height - this.height
+  }
+
+  public setSpritesheetFrames(
+    spriteSheetFrameX: number,
+    spriteSheetFrameY: number
+  ) {
+    this.spriteSheetFrameX = spriteSheetFrameX
+    this.spriteSheetFrameY = spriteSheetFrameY
+  }
+
+  public setState(stateName: StateName) {
+    this.currentState = this.states[stateName]
+    this.currentState.enter()
   }
 }
---
+++ src/game/State.ts
@@ -0,0 +1,89 @@
+import { ACTION, InputHandler } from './InputHandler'
+
+export type StateName = 'sitting' | 'running' | 'jumping'
+
+export interface Player {
+  setSpritesheetFrames: (
+    spriteSheetFrameX: number,
+    spriteSheetFrameY: number
+  ) => void
+  setState: (stateName: StateName) => void
+  isOnGround: boolean
+}
+
+export abstract class State {
+  constructor(public stateName: StateName, protected player: Player) {}
+
+  abstract enter(): void
+  abstract handleInput(inputHandler: InputHandler): void
+}
+
+export class SittingState extends State {
+  constructor(player: Player) {
+    super('sitting', player)
+  }
+
+  enter() {
+    this.player.setSpritesheetFrames(0, 5)
+  }
+
+  handleInput(inputHandler: InputHandler) {
+    if (this.player.isOnGround && inputHandler.getIsActionActive(ACTION.LEFT)) {
+      this.player.setState('running')
+    } else if (
+      this.player.isOnGround &&
+      inputHandler.getIsActionActive(ACTION.RIGHT)
+    ) {
+      this.player.setState('running')
+    }
+
+    if (this.player.isOnGround && inputHandler.getIsActionActive(ACTION.UP)) {
+      this.player.setState('jumping')
+    }
+  }
+}
+
+export class RunningState extends State {
+  constructor(player: Player) {
+    super('running', player)
+  }
+
+  enter() {
+    this.player.setSpritesheetFrames(0, 3)
+  }
+
+  handleInput(inputHandler: InputHandler) {
+    if (
+      !inputHandler.getIsActionActive(ACTION.LEFT) &&
+      !inputHandler.getIsActionActive(ACTION.RIGHT)
+    ) {
+      this.player.setState('sitting')
+    }
+
+    if (this.player.isOnGround && inputHandler.getIsActionActive(ACTION.UP)) {
+      this.player.setState('jumping')
+    }
+  }
+}
+
+export class JumpingState extends State {
+  constructor(player: Player) {
+    super('jumping', player)
+  }
+
+  enter() {
+    this.player.setSpritesheetFrames(0, 1)
+  }
+
+  handleInput(inputHandler: InputHandler) {
+    if (
+      this.player.isOnGround &&
+      (inputHandler.getIsActionActive(ACTION.LEFT) ||
+        inputHandler.getIsActionActive(ACTION.RIGHT))
+    ) {
+      this.player.setState('sitting')
+    } else if (this.player.isOnGround) {
+      this.player.setState('sitting')
+    }
+  }
+}
